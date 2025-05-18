603c1edc0ad0ee7672e319238e7c67a7dba13c0e
Iver Marl Dumalag <138662321+IverDumalag@users.noreply.github.com>
2025-05-18 16:40:34 +0800

    Update users.php

--- api/users.php
+++ api/users.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../config.php';
 header('Content-Type: application/json');
 
 // Example: GET list of users
-$result = $mysqli->query("SELECT id, name, email FROM tbl_users");
+$result = $mysqli->query("SELECT * FROM tbl_users");
 $users = [];
 while ($row = $result->fetch_assoc()) {
     $users[] = $row;
