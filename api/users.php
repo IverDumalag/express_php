17d99d67dfefdf4d2b78dc82bdcb70cd2489dad7
Iver Marl Dumalag <138662321+IverDumalag@users.noreply.github.com>
2025-05-18 14:43:18 +0800

    try-tbl_users

--- api/users.php
+++ api/users.php
@@ -3,7 +3,7 @@ require 'config.php';
 header('Content-Type: application/json');
 
 // Example: GET list of users
-$result = $mysqli->query("SELECT id, name, email FROM users");
+$result = $mysqli->query("SELECT id, name, email FROM tbl_users");
 $users = [];
 while ($row = $result->fetch_assoc()) {
     $users[] = $row;
