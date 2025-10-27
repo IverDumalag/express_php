cfbf65ba648882dc8bc7a8e5f79727b36ca7d031
Iver Marl Dumalag <138662321+IverDumalag@users.noreply.github.com>
2025-10-27 19:39:16 +0800

    Revert "Enhance CORS handling in config and users API"
    This reverts commit 5d967c2197570f4c0ff3a3464deaf7a74f02ac16.

--- api/users.php
+++ api/users.php
@@ -7,7 +7,7 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: *");
 }
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: content-type, Content-Type, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
